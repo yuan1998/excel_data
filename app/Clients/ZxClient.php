fd1f23d5a776540b0cac404820ed5ff2b030bf3d
yuan1998 <chizhiyueshu@gmail.com>
2020-09-05 10:14:27 +0800

    fix zx account

--- app/Clients/ZxClient.php
+++ app/Clients/ZxClient.php
@@ -8,12 +8,12 @@ class ZxClient extends BaseClient
     public static $cookie_name = '172.16.8.880_AdminContext_';
     public static $base_url = 'http://172.16.8.8/';
     public static $domain = '172.16.8.8';
-    public static $companyApi = true;
+    public static $companyApi = false;
     public static $mediaSourceType = '9295C7B6F93E4E51A9C09E1C2198CCB5';
 
     public static $account = [
-        'username' => '7016',
-        'password' => 'hm2018',
+        'username' => '7010',
+        'password' => 'hm2020',
     ];
 
     public static $baseAccount = [
