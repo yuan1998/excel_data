3e4852fb8dbe3d34e46be8a62e3d4787e683b309
yuan1998 <chizhiyueshu@gmail.com>
2020-09-30 15:27:53 +0800

    修复之前table 文件 bug

--- database/migrations/2019_11_13_210650_change_weibo_spend_data_table_add_new_field.php
+++ database/migrations/2019_11_13_210650_change_weibo_spend_data_table_add_new_field.php
@@ -14,18 +14,18 @@ class ChangeWeiboSpendDataTableAddNewField extends Migration
     public function up()
     {
         Schema::table('weibo_spends', function (Blueprint $table) {
-            $table->string('comment_count')->nullable();
-            $table->string('comment_count_fans')->nullable();
-            $table->string('follow_count')->nullable();
-            $table->string('follow_count_fans')->nullable();
-            $table->string('form_count')->nullable();
-            $table->string('form_count_fans')->nullable();
-            $table->string('like_count')->nullable();
-            $table->string('like_count_fans')->nullable();
-            $table->string('share_count')->nullable();
-            $table->string('share_count_fans')->nullable();
-            $table->string('start_count')->nullable();
-            $table->string('start_count_fans')->nullable();
+//            $table->string('comment_count')->nullable();
+//            $table->string('comment_count_fans')->nullable();
+//            $table->string('follow_count')->nullable();
+//            $table->string('follow_count_fans')->nullable();
+//            $table->string('form_count')->nullable();
+//            $table->string('form_count_fans')->nullable();
+//            $table->string('like_count')->nullable();
+//            $table->string('like_count_fans')->nullable();
+//            $table->string('share_count')->nullable();
+//            $table->string('share_count_fans')->nullable();
+//            $table->string('start_count')->nullable();
+//            $table->string('start_count_fans')->nullable();
         });
     }
 
@@ -37,18 +37,18 @@ class ChangeWeiboSpendDataTableAddNewField extends Migration
     public function down()
     {
         Schema::table('weibo_spends', function (Blueprint $table) {
-            $table->dropColumn('comment_count');
-            $table->dropColumn('comment_count_fans');
-            $table->dropColumn('follow_count');
-            $table->dropColumn('follow_count_fans');
-            $table->dropColumn('form_count');
-            $table->dropColumn('form_count_fans');
-            $table->dropColumn('like_count');
-            $table->dropColumn('like_count_fans');
-            $table->dropColumn('share_count');
-            $table->dropColumn('share_count_fans');
-            $table->dropColumn('start_count');
-            $table->dropColumn('start_count_fans');
+//            $table->dropColumn('comment_count');
+//            $table->dropColumn('comment_count_fans');
+//            $table->dropColumn('follow_count');
+//            $table->dropColumn('follow_count_fans');
+//            $table->dropColumn('form_count');
+//            $table->dropColumn('form_count_fans');
+//            $table->dropColumn('like_count');
+//            $table->dropColumn('like_count_fans');
+//            $table->dropColumn('share_count');
+//            $table->dropColumn('share_count_fans');
+//            $table->dropColumn('start_count');
+//            $table->dropColumn('start_count_fans');
         });
     }
 }
