366cb96407a3ffab00bdb338ad9c10f453c5a139
刘云 <liuyun@liuyundeMac-mini.local>
2023-04-10 13:07:01 +0800

    change zx url

--- app/Clients/ZxClient.php
+++ app/Clients/ZxClient.php
@@ -5,9 +5,9 @@ namespace App\Clients;
 class ZxClient extends BaseClient
 {
     public static $type = 'zx';
-    public static $cookie_name = '172.16.8.880_AdminContext_';
-    public static $base_url = 'http://172.16.8.8/';
-    public static $domain = '172.16.8.8';
+    public static $cookie_name = '172.16.8.280_AdminContext_';
+    public static $base_url = 'http://172.16.8.2/';
+    public static $domain = '172.16.8.2';
     public static $companyApi = false;
     public static $mediaSourceType = '9295C7B6F93E4E51A9C09E1C2198CCB5';
 
